30074948cb4d2ee2ac890e90a01ac47e9dfff25e
Lucas Magno <lucasmagno695@gmail.com>
2022-04-04 15:20:40 -0300

    FormatacaoMoeda

---
+++ FormatacaoNumber.java
@@ -0,0 +1,29 @@
+package exerciciosLogica.basico;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class FormatacaoNumber {
+
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		double valor = input.nextDouble();
+		BigDecimal payment = new BigDecimal(valor);
+		
+		Locale us = Locale.US;
+		Locale china = Locale.CHINA;
+		Locale france = Locale.FRANCE;
+		
+		NumberFormat US = NumberFormat.getCurrencyInstance(us);
+		NumberFormat CH = NumberFormat.getCurrencyInstance(china);
+		NumberFormat FR = NumberFormat.getCurrencyInstance(france);
+		
+		System.out.println("US: " + US.format(payment));
+		System.out.printf("India: %s\n",NumberFormat.getCurrencyInstance(new Locale("en", "in")).format(payment));
+		System.out.println("China: " + CH.format(payment));
+		System.out.println("France: " + FR.format(payment));
+	}
+}
